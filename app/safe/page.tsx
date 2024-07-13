4b09bc191f313ea85afd705c79c6d6301512e249
Iceywil <scherrer.wilfried@gmail.com>
2024-07-13 21:24:52 +0200

    fixes

--- app/safe/page.tsx
+++ app/safe/page.tsx
@@ -14,118 +14,122 @@ import { useAccount } from "wagmi";
 import { useState } from "react";
 
 export default function Home() {
-  const { address, isConnecting, isDisconnected } = useAccount();
-  const [passkey, setPasskey] = useState(false);
-  const [messages, setMessages] = useState<string[]>([]);
+	const { address, isConnecting, isDisconnected } = useAccount();
+	const [passkey, setPasskey] = useState(false);
+	const [messages, setMessages] = useState<string[]>([]);
 
-  const handleClick = (event: React.MouseEvent) => {
-    event.preventDefault();
-    setPasskey(!passkey);
-  };
+	const handleClick = (event: React.MouseEvent) => {
+		event.preventDefault();
+		setPasskey(!passkey);
+	};
 
-  const handleWorldID = (event: React.MouseEvent) => {
-    event.preventDefault();
-    setMessages((prevMessages) => [...prevMessages, "hello"]);
-  };
+	const handleWorldID = (event: React.MouseEvent) => {
+		event.preventDefault();
+		setMessages((prevMessages) => [...prevMessages, "hello"]);
+	};
 
-  return (
-    <main className="bg-black">
-      <Navbar />
-      <h1 className="text-4xl py-10 font-bold text-center text-white">Dashboard</h1>
+	return (
+		<main className="bg-black">
+			<Navbar />
+			<h1 className="text-4xl py-10 font-bold text-center text-white">Dashboard</h1>
 
-      <div className="flex justify-center px-10 pt-10 space-x-4 max-h-[26em]">
-        <section className="grid grid-cols-1 gap-6 md:grid-cols-3">
-          <Card>
-            <CardHeader>
-              <CardTitle className="pl-2">
-                <span className="text-[hsl(136,100%,66%)]">Create</span> a Smart Wallet with{" "}
-                <span className="text-[#35c184]">Safe</span>
-              </CardTitle>
-            </CardHeader>
-            <CardContent>
-              <form className="grid gap-4">
-                <div className="grid gap-2">
-                  <Label className="pl-2" htmlFor="name">
-                    Owner Wallet Address
-                  </Label>
-                  <div className="pl-2">{address}</div>
-                </div>
-                <div className="text-left pl-2">
-                  <Button
-                    className={`border font-bold${passkey ? "bg-[#35c184]" : ""}`}
-                    type="submit"
-                    onClick={handleClick}
-                  >
-                    Enable PassKey
-                  </Button>
-                </div>
-                <div className="text-left pl-2">
-                  <Button className="border font-bold" type="submit" onClick={handleWorldID}>
-                    Add a World ID Recovery
-                  </Button>
-                </div>
-                {messages.map((message, index) => (
-                  <p key={index} className="pl-2 text-white">
-                    {message}
-                  </p>
-                ))}
-				<div className="grid pl-2">
-                <Button className="font-bold text-black bg-white" type="submit">
-                  Deploy
-                </Button>
-				</div>
-              </form>
-            </CardContent>
-          </Card>
-          <Card>
-            <CardHeader>
-              <CardTitle>
-                <span className="text-[#50ff7f]">Add Recovery</span> on an Existing{" "}
-                <span className="text-[#35c184]">Safe</span> Smart Wallet
-              </CardTitle>
-            </CardHeader>
-            <CardContent>
-              <form className="grid gap-4">
-                <div className="grid gap-2">
-                  <Label htmlFor="name">Smart Wallet Address</Label>
-                  <Input id="name" placeholder="0x...." />
-                </div>
+			<div className="flex justify-center px-10 pt-10 space-x-4 max-h-[26em]">
+				<section className="grid grid-cols-1 gap-6 md:grid-cols-3">
+					<Card className="justify-around">
+						<CardHeader>
+							<CardTitle className="pl-2">
+								<span className="text-[hsl(136,100%,66%)]">Create</span> a Smart Wallet with{" "}
+								<span className="text-[#35c184]">Safe</span>
+							</CardTitle>
+						</CardHeader>
+						<CardContent>
+							<form className="grid gap-4">
+								<div className="grid gap-2">
+									<Label className="pl-2" htmlFor="name">
+										Owner Wallet Address
+									</Label>
+									<div className="pl-2">{address}</div>
+								</div>
+								<div className="text-left pl-2">
+									<Button
+										className={`border font-bold ${passkey ? "bg-[#35c184]" : ""}`}
+										type="submit"
+										onClick={handleClick}
+									>
+										Enable PassKey
+									</Button>
+								</div>
+								<div className="text-left pl-2">
+									<Button className="border font-bold" type="submit" onClick={handleWorldID}>
+										Add a World ID Recovery
+									</Button>
+								</div>
+								{messages.map((message, index) => (
+									<p key={index} className="pl-2 text-white">
+										{message}
+									</p>
+								))}
+								<div className="grid pl-2">
+									<Button className="font-bold text-white bg-green-600" type="submit">
+										Deploy
+									</Button>
+								</div>
+							</form>
+						</CardContent>
+					</Card>
+					<Card>
+						<CardHeader>
+							<CardTitle>
+								<span className="text-[#50ff7f]">Add Recovery</span> on an Existing{" "}
+								<span className="text-[#35c184]">Safe</span> Smart Wallet
+							</CardTitle>
+						</CardHeader>
+						<CardContent>
+							<form className="grid gap-4">
+								<div className="grid gap-2">
+									<Label htmlFor="name">Smart Wallet Address</Label>
+									<Input id="name" placeholder="0x...." />
+								</div>
 
-                <Button className="text-[#35c184] border" type="submit">
-                  Submit
-                </Button>
-              </form>
-            </CardContent>
-          </Card>
-          <Card>
-            <CardHeader>
-              <CardTitle>
-                <span className="text-[#50ff7f]">Recover</span> Smart Wallet with{" "}
-                <span className="text-[#35c184]">World ID</span>
-              </CardTitle>
-            </CardHeader>
-            <CardContent>
-              <form className="grid gap-4">
-                <div className="grid gap-2">
-                  <Label htmlFor="name">Name</Label>
-                  <Input id="name" placeholder="Enter your name" />
-                </div>
-                <div className="grid gap-2">
-                  <Label htmlFor="email">Email</Label>
-                  <Input id="email" type="email" placeholder="Enter your email" />
-                </div>
-                <div className="grid gap-2">
-                  <Label htmlFor="message">Message</Label>
-                  <Textarea id="message" placeholder="Enter your message" />
-                </div>
-                <Button className="border" type="submit">
-                  Recover
-                </Button>
-              </form>
-            </CardContent>
-          </Card>
-        </section>
-      </div>
-    </main>
-  );
+								<div className="grid">
+									<Button className="font-bold text-white bg-green-600" type="submit">
+										Add
+									</Button>
+								</div>
+							</form>
+						</CardContent>
+					</Card>
+					<Card>
+						<CardHeader>
+							<CardTitle>
+								<span className="text-[#50ff7f]">Recover</span> Smart Wallet with{" "}
+								<span className="text-[#35c184]">World ID</span>
+							</CardTitle>
+						</CardHeader>
+						<CardContent>
+							<form className="grid gap-4">
+								<div className="grid gap-2">
+									<Label htmlFor="name">Name</Label>
+									<Input id="name" placeholder="Enter your name" />
+								</div>
+								<div className="grid gap-2">
+									<Label htmlFor="email">Email</Label>
+									<Input id="email" type="email" placeholder="Enter your email" />
+								</div>
+								<div className="grid gap-2">
+									<Label htmlFor="message">Message</Label>
+									<Textarea id="message" placeholder="Enter your message" />
+								</div>
+								<div className="grid">
+									<Button className="font-bold text-white bg-green-600" type="submit">
+										Recover
+									</Button>
+								</div>
+							</form>
+						</CardContent>
+					</Card>
+				</section>
+			</div>
+		</main>
+	);
 }
